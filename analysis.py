2dbc71b455c0459a82a57d14cc32deaad78627f2
Gianmarc Grazioli <gianmarc@Gianmarcs-Work-MacBook.local>
2022-03-21 20:49:35 -0700

    updated RDF for solid and hollow sphere

--- analysis.py
+++ analysis.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 from mathTools import PeriodicVectorCorrection
     
-def GetRDF(ensemble, radSteps=20, sampleFrac=.4):
+def GetRDF_SolidSphere(ensemble, radSteps=20, sampleFrac=.4):
     '''
     Parameters
     ----------
@@ -52,6 +52,56 @@ def GetRDF(ensemble, radSteps=20, sampleFrac=.4):
     for idx,r in enumerate(radList):
         if idx % 5 == 0: print("RDF calculation", '{0:.0f}'.format(idx/len(radList)*100), "% complete.")
         nExpect = molarDensity * (ensemble.dim + 1)/3. * np.pi * r ** ensemble.dim
+        RDF.append( GetOneRadius(ensemble, r) / nExpect)
+    out = zip(np.linspace(0, ensemble.sideLen/2, radSteps), RDF)
+    return (pd.DataFrame(out, columns=['r','RDF']))
+
+def GetRDF_HollowSphere(ensemble, radSteps=20, sampleFrac=.4, dr=.05):
+    '''
+    Parameters
+    ----------
+    ensemble : Ensemble
+        Ensemble class.
+    radSteps : int, optional
+        Number of steps in radius value for RDF. The default is 20.
+    sampleFrac : float, optional
+        Fraction of atoms to select for RDF (<= 1). The default is .2.
+
+    Returns
+    -------
+    Pandas DataFrame
+        Radial distribution function (RDF). 
+    '''
+    # Returns a radial distribution function for the ensemble ranging 
+    # from zero to half the length of the side of the container 
+    sampleNum = round(sampleFrac * ensemble.nMol)
+    def CountMols(ensemble, step, moID, radius):
+        m1 = ensemble.trajectory[step][moID] 
+        ct = 0
+        for m,mol in enumerate(ensemble.trajectory[step]):
+            diff = mol - m1 
+            if ensemble.periodic:
+                diff = PeriodicVectorCorrection(diff, ensemble.sideLen)
+            if ((np.sqrt(sum((diff)**2)) <= radius) and 
+                (np.sqrt(sum((diff)**2)) >= (radius-dr)) and 
+                (moID != m) ):
+                ct += 1
+        return(ct)
+    def GetOneRadius(ensemble, radius, sampleFrac=sampleFrac):
+        out = 0. 
+        for t in range(0, len(ensemble.trajectory)):
+            sampledIndices = np.random.choice(list(range(ensemble.nMol)), sampleNum, replace=False) 
+            for m in sampledIndices:
+                out += CountMols(ensemble, t, m, radius)
+        return (out / (len(ensemble.trajectory) * sampleNum)) 
+    RDF = []
+    molarDensity = ensemble.nMol / (ensemble.sideLen ** ensemble.dim)
+    radList = np.linspace(ensemble.sideLen/9999999, ensemble.sideLen/2., radSteps)
+    for idx,r in enumerate(radList):
+        if idx % 5 == 0: print("RDF calculation", '{0:.0f}'.format(idx/len(radList)*100), "% complete.")
+        bigSphere = molarDensity * (ensemble.dim + 1)/3. * np.pi * r ** ensemble.dim
+        littleSphere = molarDensity * (ensemble.dim + 1)/3. * np.pi * (r - dr) ** ensemble.dim
+        nExpect = bigSphere - littleSphere
         RDF.append( GetOneRadius(ensemble, r) / nExpect)
     out = zip(np.linspace(0, ensemble.sideLen/2, radSteps), RDF)
     return (pd.DataFrame(out, columns=['r','RDF']))
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ Created on Sun Nov 28 14:43:33 2021
 
 @author: Gianmarc Grazioli 
 """ 
-
+ 
 from classes import Ensemble
 from dynamics import RunDynamics
 import visualizationTools as vis
-from analysis import GetRDF
+from analysis import GetRDF_HollowSphere
 import time
 
 startTime = time.time()
@@ -19,12 +19,12 @@ T_init = 1.0 # initial temperature
 molecules = Ensemble(nMol = 64, sideLen=5, dim=3, sigma=1.0, epsilon=1.0, buffer=.75, periodic=True, useLJ=True, temperature=T_init)              
 vis.plotLJpotential(molecules)
             
-trajectory = RunDynamics(molecules, nSteps=1000, saveFreq=5)
+trajectory = RunDynamics(molecules, nSteps=1000, saveFreq=3)
 #vis.makeTrajMovie2D(molecules.trajectory, molecules.sideLen) # best for 2D liquid simulation
 vis.plotKEandPEandTotal("KE.csv", "PE.csv")
 vis.writeXYZ(molecules, "trajectory.xyz")
 
-RDF = GetRDF(molecules, radSteps=40) 
+RDF = GetRDF_HollowSphere(molecules, radSteps=50) 
 RDF.plot(x='r', y='RDF')
 
 executionTime = (time.time() - startTime)
